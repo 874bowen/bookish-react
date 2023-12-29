e948b2f9f5789250d3596267f716d5079fdf1eed
Ivan Bowen <bowenivan16@gmail.com>
2023-12-29 07:10:21 +0300

    update App and test to use stub-server

--- cypress/e2e/bookish.spec.cy.ts
+++ cypress/e2e/bookish.spec.cy.ts
@@ -1,6 +1,48 @@
+import axios from "axios";
+
 describe('Bookish application', function() {
+  // cleanup stub before and after tests
+  beforeEach(async() => {
+    return axios
+      .delete(`http://localhost:8080/books?_cleanup=true`)
+      .catch(err => err)
+  })
+
+  afterEach(async() => {
+    return axios
+      .delete(`http://localhost:8080/books?_cleanup=true`)
+      .catch(err => err)
+  })
+
+  // add items to stub server before tests and after ensuring there is no data
+  beforeEach(()=> {
+    const books = [
+      { "name": "Refactoring", "id": 1 },
+      { "name": "Domain-driven design", "id": 2 },
+      { "name": "Building Microservices", "id": 3 }
+    ]
+
+    return books.map((book) => {
+      axios
+        .post(`http://localhost:8080/books`, book, 
+          { headers: { 'Content-Type': 'application/json' } })
+        .catch(err => err)
+      })
+  })
   it('Visits the bookish', function() {
     cy.visit('http://localhost:3000/');
     cy.get('h2[data-test="heading"]').contains('Bookish')
+  })
+
+  it('Shows a book list', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('div[data-test="book-list"]').should('exist');
+    cy.get('div.book-item').should((books) => {
+      expect(books).to.have.length(books.length);
+      const titles = [...books].map(x => x.querySelector('h2')?.innerHTML);
+      expect(titles).to.deep.equal(
+        ['Refactoring', 'Domain-driven design', 'Building Microservices']
+      )
+    });
   })
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import { Typography } from '@mui/material';
 import './App.css';
+import { Book } from './types';
+import BookList from './BookList';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  console.log(`${process.env.REACT_APP_LOCAL_SERVER_URL}`);
+  const [books, setBooks] = useState<Book[]>([])
+  // const books:Book[] = [{ name: 'Refactoring' }, { name: 'Domain-driven design' }]
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/books').then((res) => {
+      if (res.status === 200) setBooks(res.data)
+    }).catch(error => console.log(error)
+    )
+  }, [])
   return (
-    <Typography variant='h2' component='h2' data-test='heading'>
-      Bookish
-    </Typography>
+    <>
+      <Typography variant='h2' component='h2' data-test='heading'>
+        Bookish
+      </Typography>
+      <Typography>
+        <BookList books={books} />
+      </Typography>
+    </>
   );
 }
 
