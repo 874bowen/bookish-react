42baf772543258966dd543d63e55cb72c8188b07
Ivan Bowen <bowenivan16@gmail.com>
2024-01-05 21:43:57 +0300

    code refactor

--- src/hooks/useBook.ts
+++ src/hooks/useBook.ts
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react"
 import { Book } from "../types"
 import { useParams } from "react-router"
 
-export const useBooks = () => {
+export const useBook = () => {
     const { id } = useParams<string>()
     const [book, setBook] = useState<Book>()
     const [loading, setLoading] = useState<boolean>(false)
