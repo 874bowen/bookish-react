32ef34125a926aa228af1e7915d515e49f9381a2
Ivan Bowen <bowenivan16@gmail.com>
2024-01-06 20:56:31 +0300

    add styles

--- src/components/BookList/BookList.tsx
+++ src/components/BookList/BookList.tsx
@@ -1,13 +1,47 @@
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  Grid,
+  Typography,
+} from "@mui/material";
 import { Book } from "../../types";
+import { Link } from "react-router-dom";
 
 const BookList = ({ books }: { books: Book[] }) => (
   <div data-test="book-list">
-    {books.map((book) => (
-      <div className="book-item" key={book.name}>
-        <h2>{book.name}</h2>
-        <a href={`/books/${book.id}`}>View Details</a>
-      </div>
-    ))}
+    <Grid container spacing={3}>
+      {books.map((book) => (
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          md={4}
+          key={`$${book.name}-${book.id}`}
+          className="book-item"
+        >
+          <Card>
+            <CardActionArea>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {book.name}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {book.description}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button size="small" color="primary">
+                <Link to={`/books/${book.id}`}>View Details</Link>
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
   </div>
 );
 
---
+++ src/components/BookList/BookListContainer.tsx
@@ -0,0 +1,19 @@
+import { Typography } from "@mui/material";
+import BookList from "./BookList";
+import { useBooks } from "../../hooks/useBooks";
+
+const BookListContainer = () => {
+  const { loading, error, books } = useBooks();
+
+  if (loading) return <p>Loading ... </p>;
+
+  if (error) return <p>Error ... </p>;
+
+  return (
+    <Typography component="div">
+      <BookList books={books} />
+    </Typography>
+  );
+};
+
+export default BookListContainer;
