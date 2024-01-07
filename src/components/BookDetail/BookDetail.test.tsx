a16f666f36498f31632dea69060b3608efcd514a
Ivan Bowen <bowenivan16@gmail.com>
2024-01-07 19:23:27 +0300

    test for longer description by making a view more a tag

--- src/components/BookDetail/BookDetail.test.tsx
+++ src/components/BookDetail/BookDetail.test.tsx
@@ -17,26 +17,51 @@ describe("Book Detail", () => {
   it("renders description", () => {
     const props = {
       book: {
-        description: "Martin Fowler's Refactoring defined core ideas and techniques ...",
+        description:
+          "Martin Fowler's Refactoring defined core ideas and techniques ...",
         name: "Refactoring",
-        id: 1
-      }
-    }
+        id: 1,
+      },
+    };
 
-    render(<BookDetail {...props} />)
-    const description = screen.getByText(/martin fowler's refactoring defined core ideas and techniques \.\.\./i)
-    expect(description).toBeInTheDocument()
-  })
+    render(<BookDetail {...props} />);
+    const description = screen.getByText(
+      /martin fowler's refactoring defined core ideas and techniques \.\.\./i
+    );
+    expect(description).toBeInTheDocument();
+  });
 
   it("renders title when no description is given", () => {
     const props = {
       book: {
         name: "Refactoring",
         id: 1,
-      }
-    }
-    render(<BookDetail {...props} />)
-    const description = screen.getByTestId("book-description")
-    expect(description).toHaveTextContent(props.book.name)
-  })
+      },
+    };
+    render(<BookDetail {...props} />);
+    const description = screen.getByTestId("book-description");
+    expect(description).toHaveTextContent(props.book.name);
+  });
+
+  it("renders show *more* link when description ecxeeds 300 characters", () => {
+    const props = {
+      book: {
+        name: "Lorem Ipsum",
+        id: 1,
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae soluta exercitationem laudantium animi! Quo laboriosam nam id porro? Qui nostrum enim iure voluptate suscipit ullam delectus quas adipisci magni similique? Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae soluta exercitationem laudantium animi! Quo laboriosam nam id porro? Qui nostrum enim iure voluptate suscipit ullam delectus quas adipisci magni similique?",
+      },
+    };
+    render(<BookDetail {...props} />);
+    const link = screen.getByText(/view more/i);
+    expect(link).toBeInTheDocument();
+    const description = screen.getByTestId("book-description");
+    expect(description).toHaveTextContent(
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae soluta exercitationem laudantium animi! Quo laboriosam nam id porro? Qui nostrum enim iure voluptate suscipit ullam delectus quas adipisci magni similique? Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae soluta exe ... "
+    );
+  });
+
+  /**
+   * TODO: test for - shows all the descrption content when the view more link is clicked
+   */
 });
